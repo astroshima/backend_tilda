5aa028c3a530e9674c93058d164547335c3e626b
astroshima <astroshima@users.noreply.github.com>
2020-03-12 08:12:55 +0100

    changed common endpoint (to: blog) for whole Blog API

--- mojblog/api/__init__.py
+++ mojblog/api/__init__.py
@@ -1,11 +1,11 @@
 from freenit.api import register_endpoints
 
 def create_api(app):
-    from .blog_post import blueprint as blog_post
+    from .blog_post import blueprint as blog
     register_endpoints(
         app,
         '/api/v0',
         [
-            blog_post,
+            blog,
         ]
     )
--- mojblog/api/blog_post.py
+++ mojblog/api/blog_post.py
@@ -2,7 +2,7 @@ from freenit.api.methodviews import MethodView
 from flask_smorest import Blueprint
 from ..schemas.blog_post import BlogPostSchema
 
-blueprint = Blueprint('blogs', 'blogs')
+blueprint = Blueprint('blog', 'blog')
 
 @blueprint.route('/post', endpoint='create_blog_post')
 class CreateBlogPostAPI(MethodView):
