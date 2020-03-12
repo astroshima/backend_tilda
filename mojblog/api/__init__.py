164c9541dd073175db55f1b7518de4ccf62121e5
astroshima <astroshima@users.noreply.github.com>
2020-03-12 08:23:17 +0100

    refactored:    mojblog/api/blog_post.py -> mojblog/api/blog.py

--- mojblog/api/__init__.py
+++ mojblog/api/__init__.py
@@ -1,7 +1,7 @@
 from freenit.api import register_endpoints
 
 def create_api(app):
-    from .blog_post import blueprint as blog
+    from .blog import blueprint as blog
     register_endpoints(
         app,
         '/api/v0',
