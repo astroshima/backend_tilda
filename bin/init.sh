9b1b731a13f72794495370c53b6970aa61bbaf6c
astroshima <astroshima@users.noreply.github.com>
2020-04-17 14:48:33 +0200

    new scripts

--- bin/init.sh
+++ bin/init.sh
@@ -2,18 +2,19 @@
 
 
 export OFFLINE=${OFFLINE:=no}
-export BIN_DIR=`dirname $0`
+BIN_DIR=`dirname $0`
 . ${BIN_DIR}/common.sh
-#echo $PY_VERSION
+
+
 if [ "${OFFLINE}" = "yes" ]; then
   setup no
 else
   setup
 fi
-
-
 if [ ! -e "${BIN_DIR}/../migrations/main/001_initial.py" ]; then
   flask migration create initial
-  flask migration run
 fi
+
+
+flask migration run
 flask admin create
