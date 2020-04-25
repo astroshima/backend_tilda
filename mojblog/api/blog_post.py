725b601bcb588ecf97e5db09ff40d3ab7ea803c9
astroshima <astroshima@users.noreply.github.com>
2020-04-25 19:19:42 +0200

    initial commit: API class removed

--- mojblog/api/blog_post.py
+++ mojblog/api/blog_post.py
@@ -1,4 +1,3 @@
-from freenit.api.methodviews import MethodView
 from flask_smorest import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from ..schemas.blog_post import BlogPostSchema
@@ -7,19 +6,18 @@ from ..models.user import User
 
 blueprint = Blueprint('blogpost', 'blogpost')
 
-@blueprint.route('', endpoint='create_blog_post')
-class CreateBlogPostAPI(MethodView):
-    @jwt_required
-    @blueprint.response(BlogPostSchema)
-    @blueprint.arguments(BlogPostSchema)
-    def post(self, args):
-        '''Create blog post'''
-        blogPost = BlogPost(**args)
-        userId = get_jwt_identity()
-        try:
-            user = User.get(id=userId)
-        except User.DoesNotExist:
-            abort(404, message='User not found')
-        blogPost.author = user
-        blogPost.save()
-        return blogPost
+@blueprint.route('', methods=['POST'], endpoint='create_blog_post')
+@jwt_required
+@blueprint.response(BlogPostSchema)
+@blueprint.arguments(BlogPostSchema)
+def createBlogPost(args):
+    '''Create blog post'''
+    blogPost = BlogPost(**args)
+    userId = get_jwt_identity()
+    try:
+        user = User.get(id=userId)
+    except User.DoesNotExist:
+        abort(404, message='User not found')
+    blogPost.author = user
+    blogPost.save()
+    return blogPost
