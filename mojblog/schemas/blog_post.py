d6d6265022afad55c7d2eff0cb0e33b626858132
astroshima <astroshima@users.noreply.github.com>
2020-05-07 22:59:31 +0200

    little refactoring of blog_post schema

--- mojblog/schemas/blog_post.py
+++ mojblog/schemas/blog_post.py
@@ -8,10 +8,10 @@ class BlogPostSchema(BaseSchema):
     id = fields.Integer(description='ID', dump_only=True)
     title = fields.String(description='Title', required=True)
     content = fields.String(description='Content')
-    author = fields.Nested(UserSchema, dump_only=True)
     date = fields.DateTime(
         description='Time when blog post was created',
         dump_only=True,
     )
+    author = fields.Nested(UserSchema, dump_only=True)
 
 PageOutSchema(BlogPostSchema, sys.modules[__name__])
