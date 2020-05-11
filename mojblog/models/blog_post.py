bee9af67bef4e3502ebefc2555bc8f39fa79e917
astroshima <astroshima@users.noreply.github.com>
2020-05-11 16:11:33 +0200

    added slug & published fields in BlogPost model

--- mojblog/models/blog_post.py
+++ mojblog/models/blog_post.py
@@ -1,14 +1,16 @@
 import datetime
+from peewee import DateTimeField, ForeignKeyField, TextField, BooleanField
 from freenit.db import db
-from peewee import DateTimeField, ForeignKeyField, TextField
 from .user import User
 
 Model = db.Model
 
 class BlogPost(Model):
+    slug = TextField()
     title = TextField()
     content = TextField()
     date = DateTimeField(
         default=datetime.datetime.utcnow
     )
-    author = ForeignKeyField(User, backref='blogpost')  
+    author = ForeignKeyField(User, backref='blogpost')
+    published = BooleanField()
