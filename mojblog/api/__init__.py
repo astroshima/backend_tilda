519f3ab27af7ee136154856debcb503e35a94729
astroshima <astroshima@users.noreply.github.com>
2020-06-02 18:34:06 +0200

    Added SearchBlogPostsByTitleAPI

--- mojblog/api/__init__.py
+++ mojblog/api/__init__.py
@@ -2,10 +2,12 @@ from freenit.api import register_endpoints
 
 def create_api(app):
     from .blog_post import blueprint as blogPost
+    from .blog_post_search import blueprint as blogPostSearch
     register_endpoints(
         app,
         '/api/v0',
         [
             blogPost,
+            blogPostSearch,
         ]
     )
